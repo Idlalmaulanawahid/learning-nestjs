19984d6f100fa03e8d537327cf455dc01b0ed31b
idllalmaulanawahid <idlalmaulana@gmail.com>
2020-05-02 10:32:30 +0700

    first commit

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,33 @@
+import { JwtPayload } from './jwt-payload.interface';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserRepository } from './user.repository';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthService {
+    constructor(
+        @InjectRepository(UserRepository)
+        private userRepository: UserRepository,
+        private jwtService: JwtService,
+    ) { }
+
+    async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+        return this.userRepository.signUp(authCredentialsDto);
+    }
+
+    async signIn(autCredentialsDto: AuthCredentialsDto): Promise<{ accessToken: string }> {
+        const username = await this.userRepository.validateUserPassword(autCredentialsDto);
+
+        if (!username) {
+            throw new UnauthorizedException('Invalid Credentials');
+        }
+
+        const payload: JwtPayload = { username };
+        const accessToken = await this.jwtService.sign(payload);
+
+        return { accessToken }
+
+    }
+}
